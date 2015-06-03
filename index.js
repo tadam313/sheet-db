f16661a53626dd19ed11a16b74a886f27aa0bba6
tadam313 <adam.pelle.peter@gmail.com>
2015-06-03 17:49:31 +0200

    pass options on first connect method disable falsy object exclusion

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ var SheetDb = require('./lib/sheet_db');
  * Connects and initializes the specific worksheet
  *
  * @param sheetId
+ * @param options
  * @param callback
  */
-function connect(sheetId, callback) {
-    var options = {};
-    options.version = options.version || 'v3';
+function connect(sheetId, options, callback) {
+
+    if (typeof options === 'function') {
+        callback = options;
+        options = null;
+    }
+
+    options = options || {};
+    options.version = 'v3';
 
     var sheetDb = new SheetDb(sheetId, options);
 
